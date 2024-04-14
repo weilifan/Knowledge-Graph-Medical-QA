7e6b0e058598239d2bff4e37cd690f7bcea91342
weilifan <liyixin24@gmail.com>
2024-04-14 20:40:04 +0800

    删除

--- blog/__init__.py
+++
@@ -1,6 +0,0 @@
-from .views import app
-from .models import graph
-
-# graph.schema.create_uniqueness_constraint("User", "username")
-# graph.schema.create_uniqueness_constraint("Tag", "name")
-# graph.schema.create_uniqueness_constraint("Post", "id")
